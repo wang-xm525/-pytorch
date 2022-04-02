90fc8ad76785e97f6f2dc6aa294f7b1f87391b3a
wang-xm525 <94597034+wang-xm525@users.noreply.github.com>
2022-04-02 11:00:26 +0800

    Update 线性回归从零开始实现.py

--- 线性回归从零开始实现.py
+++ 线性回归从零开始实现.py
@@ -39,6 +39,7 @@ def data_iter(batch_size, features, labels):
     # 这些样本是随机读取的，没有特定的顺序
     random.shuffle(indices)
     for i in range(0, num_examples, batch_size):
+        #每次从0开始到num_examples,跳的大小是batch_Size
         batch_indices = torch.tensor(
             indices[i: min(i + batch_size, num_examples)])
         yield features[batch_indices], labels[batch_indices]
